dd917a59e3c2871b5319d31851d2f0587c121614
BrunoAndreRamos <34645103+BrunoAndreRamos@users.noreply.github.com>
2020-06-19 15:30:54 +0100

    tr

---
+++ TetsBlazor/server/Pages/Ter.razor.designer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Radzen;
+using Radzen.Blazor;
+
+namespace TestBlazor.Pages
+{
+    public partial class TerComponent : ComponentBase
+    {
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IReadOnlyDictionary<string, dynamic> Attributes { get; set; }
+
+        [Inject]
+        protected IJSRuntime JSRuntime { get; set; }
+
+        [Inject]
+        protected NavigationManager UriHelper { get; set; }
+
+        [Inject]
+        protected DialogService DialogService { get; set; }
+
+        [Inject]
+        protected NotificationService NotificationService { get; set; }
+
+        protected override async System.Threading.Tasks.Task OnInitializedAsync()
+        {
+        }
+    }
+}
